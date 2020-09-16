f51674649c7babdcd80503df29676643822fab97
vund5 <nguyendoanvu1993@gmail.com>
2020-09-16 23:41:45 +0700

    no message

--- src/app/pages/registration-guest/registration-guest.page.ts
+++ src/app/pages/registration-guest/registration-guest.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams, NavController } from '@ionic/angular';
 import { TranslateConfigService } from '../../translate-config.service';
-
-import { PopupSelectApartmentPage } from '../popup-select-apartment/popup-select-apartment.page';
-import { PopupRegistrationSuccessPage } from '../popup-registration-success/popup-registration-success.page';
+import { ApiService } from '../../services/api/api.service';
+import { LoadingService } from '../../services/loading/loading.service';
+import { DatePipe, Location } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
+import { AlertService } from '../../services/alert/alert.service';
 
 @Component({
   selector: 'app-registration-guest',
@@ -12,30 +14,163 @@ import { PopupRegistrationSuccessPage } from '../popup-registration-success/popu
 })
 export class RegistrationGuestPage implements OnInit {
   selectedLanguage:string;
-
+  listDepartmentByID: any;
+  listDepartment: any;
+  form_apartment_id:any;
+  form_apartment_class:any;
+  form_date_time: any;
+  form_date_time_class: any;
+  list_guest: any;
+  form_note: any;
+  flag_show_hide_popup: any;
   constructor(
     public modalController: ModalController,
     private translateConfigService: TranslateConfigService,
+    private loading: LoadingService,
+    private apiService: ApiService,
+    private translate: TranslateService,
+    private location: Location,
+    private alertService: AlertService
   ) {
     this.selectedLanguage = this.translateConfigService.getDefaultLanguage();
   }
 
   ngOnInit() {
+    this.listDepartmentByID = {};
+    this.flag_show_hide_popup = false;
+    this.getListApartment();
+    this.form_apartment_id = "";
+    this.form_apartment_class = "";
+    this.form_date_time = "";
+    this.form_date_time_class = "";
+    this.form_note = "";
+    this.list_guest = {
+      guestsNumber: 1,
+      data_guest: [
+        {
+          guestsName: '',
+          guestsIdentity: '',
+          className: '',
+          classIdentity: '',
+          line: 'no-line'
+        }
+      ]
+    };
   }
-
-  async selectApartmentModal() {
-    const modal = await this.modalController.create({
-      component: PopupSelectApartmentPage,
-      cssClass: 'popupSelectApartment-page-custom'
+  getListApartment(){
+    var self = this;
+    this.loading.present();
+    this.apiService.getListUserApartment()
+      .subscribe(result => {
+        self.listDepartment = result.userApartments;
+        self.listDepartment.forEach(data =>{
+          self.listDepartmentByID[data.apartment ._id] = data;
+        });
+        self.loading.dismiss()
+    },
+    error => { 
+      self.loading.dismiss();
     });
-    return await modal.present();
   }
-
-  async popupRegistrationSuccessModal() {
-    const modal = await this.modalController.create({
-      component: PopupRegistrationSuccessPage,
-      cssClass: 'popupRegistrationSuccess-page-custom'
+  ionChangePulldown1(event){
+    if (this.form_apartment_id != '') {
+      this.form_apartment_class = 'has-input-value';
+    } else {
+      this.form_apartment_class = '';
+    }
+  }
+  ionChangeDateTime(event){
+    if (this.form_date_time != "") {
+      this.form_date_time_class = 'has-input-value';
+    } else {
+      this.form_date_time_class = "";
+    }
+  }
+  downNumberProduct() {
+    var self = this;
+    if (self.list_guest.guestsNumber > 1) {
+      self.list_guest.guestsNumber--;
+      this.list_guest.data_guest.pop();
+    }
+  }
+  upNumberProduct() {
+    var self = this;
+    if (self.list_guest.guestsNumber < 100) {
+      self.list_guest.guestsNumber++;
+      this.list_guest.data_guest.push({
+        guestsName: '',
+        guestsIdentity: '',
+        className: '',
+        classIdentity: '',
+        line: 'has-line'
+      });
+    }
+  }
+  ionchangTextBoxGuest() {
+    var self = this;
+    self.list_guest.data_guest.forEach(product => {
+      if (product.guestsName != '') {
+        product.className = 'has-input-value';
+      } else {
+        product.className = '';
+      }
+      if (product.guestsIdentity != '') {
+        product.classIdentity = 'has-input-value';
+      } else {
+        product.classIdentity = '';
+      }
     });
-    return await modal.present();
+  }
+  eventButtonRegisterNew() {
+    var self = this;
+    let dataApartment = self.listDepartmentByID[this.form_apartment_id];
+    let guestInfor = [];
+    self.list_guest.data_guest.forEach(guest => {
+      guestInfor.push({
+        guestsName: guest.guestsName,
+        guestsIdentity: guest.guestsIdentity
+      })
+    });
+    const params = {
+      // category: "",
+      title: this.translate.instant('REGISTRATION_GUEST.title'),
+      content: this.form_note,
+      campaign: dataApartment.campaign._id,
+      apartment: this.form_apartment_id,
+      // createdBy: "",
+      attachments: [],
+      type: "guests",
+      guestsNumber: self.list_guest.guestsNumber,
+      guestInfor: guestInfor,
+      guestsDate: this.form_date_time
+    };
+    this.loading.present();
+    this.apiService.addFeedback(params)
+      .subscribe(result => {
+        self.loading.dismiss();
+        self.flag_show_hide_popup = true;
+      },
+      error => {
+        self.loading.dismiss();
+        self.alertService.presentToast(this.translate.instant('ADD_REQUEST.message_add_request_fail'));
+      }
+    );
+  }
+  eventButtonClosePopup() {
+    this.flag_show_hide_popup = false;
+    this.location.back();
+  }
+  checkShowActiveButton() {
+    var check_input_inf_guest = false;
+    this.list_guest.data_guest.forEach(data => {
+      if (data.guestsName == "" || data.guestsIdentity == "") {
+        check_input_inf_guest = true;
+      }
+    });
+    if (this.form_apartment_id == "" || this.form_date_time == "" || check_input_inf_guest) {
+      return 'inactive_button';
+    } else {
+      return 'active_button';
+    }
   }
 }
