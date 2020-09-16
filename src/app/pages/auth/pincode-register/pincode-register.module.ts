e29bb777c90d0b008ba1a01a49c875488f2e932e
uyenlt2009 <uyenlt2009@gmail.com>
2020-09-16 23:28:38 +0700

    update popup-pincode-register

--- src/app/pages/auth/pincode-register/pincode-register.module.ts
+++ src/app/pages/auth/pincode-register/pincode-register.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { PincodeRegisterPage } from './pincode-register.page';
