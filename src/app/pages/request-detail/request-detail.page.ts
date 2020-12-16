e328dba4fa5651b24b7884ff860592bd633906ad
vund5 <nguyendoanvu1993@gmail.com>
2020-12-16 19:53:04 +0700

    Add code màn hình Request

--- src/app/pages/request-detail/request-detail.page.ts
+++ src/app/pages/request-detail/request-detail.page.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit, Input, ElementRef } from '@angular/core';
 import { ApiService } from '../../services/api/api.service';
-import { NavController, NavParams, Platform } from '@ionic/angular';
+import { ActionSheetController, NavController, NavParams, Platform } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 import { LoadingService } from '../../services/loading/loading.service';
 import { ConstService } from '../../utils/const.service';
 import { UtilsService } from '../../utils/utils.service';
+import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/Camera/ngx';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-request-detail',
@@ -32,9 +34,12 @@ export class RequestDetailPage implements OnInit, AfterViewInit {
   listImage: any[] = [];
   widthListScreen:number;
   showMore : false;
+  list_image_select: any;
 
   constructor(
-    
+    private camera: Camera,
+    public actionSheetController: ActionSheetController,
+    private translate: TranslateService,
     private platform: Platform,
     private loading: LoadingService,
     private apiService: ApiService,
@@ -50,7 +55,7 @@ export class RequestDetailPage implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-   
+    this.list_image_select = [];
     this.requestTitle = "";
     this.requestContent = "";
     this.currentUser = JSON.parse(localStorage.getItem('profile'));
@@ -180,6 +185,51 @@ export class RequestDetailPage implements OnInit, AfterViewInit {
     });
   }
 
+  sendAttachmentImage() {
+    if (this.list_image_select.length == 0) return;
+    var self = this;
+    // // Mock message
+    const id = Date.now().toString();
+    let listAttachments = [];
+    for(var i=0;i<self.list_image_select.length;i++){
+      listAttachments.push(self.list_image_select[i].media);
+    }
+    let newMsg = {
+      messageId: id,
+      createdBy:{
+        displayName : this.currentUser.displayName,
+        _id: this.currentUser._id,
+        avatar: this.currentUser.avatar == "" ? '../assets/icon/avatar-default.png' : this.currentUser.avatar
+      },
+      createdAt: new Date().toISOString(),
+      content: " ",
+      attachments: listAttachments
+    };
+
+    this.pushNewMsg(newMsg);
+    this.editorMsg = '';
+
+    if (!this.showEmojiPicker) {
+      this.focus();
+    }
+    const params = {
+      content: " ",
+      attachments: listAttachments
+    };
+
+    this.apiService.addFeedbackReply(self.feedbackID, params)
+    .subscribe(result => {
+      self.list_image_select = [];
+      let index = this.getMsgIndexById(id);
+      if (index !== -1) {
+        self.msgList[index].status = 'success';
+      }
+    },
+    error => {
+      self.loading.dismiss();
+    });
+  }
+
   /**
    * @name pushNewMsg
    * @param msg
@@ -217,5 +267,119 @@ export class RequestDetailPage implements OnInit, AfterViewInit {
     return string.length > length
       ? string.substring(0, length) + "..."
       : string;
+  }
+
+
+
+  // select image
+  async selectImage() {
+    const actionSheet = await this.actionSheetController.create({
+      header: this.translate.instant('COMMON.form_select_image_title'),
+      buttons: [{
+        text: this.translate.instant('COMMON.form_select_image_library'),
+        handler: () => {
+          this.checkPicturePermission(this.camera.PictureSourceType.PHOTOLIBRARY);
+        }
+      },
+      {
+        text: this.translate.instant('COMMON.form_select_image_camera'),
+        handler: () => {
+          this.checkPicturePermission(this.camera.PictureSourceType.CAMERA);
+        }
+      },
+      {
+        text: this.translate.instant('COMMON.form_select_image_cancel'),
+        role: 'cancel'
+      }
+      ]
+    });
+    await actionSheet.present();
+  }
+
+  checkPicturePermission(sourceType : PictureSourceType) {
+    this.pickImage(sourceType);
+  }
+
+  blobToFile = (theBlob: Blob, fileName:string): any => {
+    var b: any = theBlob;
+    //A Blob() is almost a File() - it's just missing the two properties below which we will add
+    b.lastModifiedDate = new Date();
+    b.name = fileName;
+
+    //Cast to a File() type
+    return theBlob;
+  }
+
+  pickImage(sourceType) {
+    var self = this;
+    const options: CameraOptions = {
+      quality: 80,
+      sourceType: sourceType,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      targetWidth: 800,
+      targetHeight: 800,
+      correctOrientation: true,
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+    
+      const base64Define = 'data:image/jpeg;base64,';
+      var binaryBlob = this.convertBase64ToBlob(base64Define+imageData);
+      const date = new Date().valueOf();
+      const formData = new FormData();
+      var fileName = "amoapp" + date + ".jpg";
+      var myFile = self.blobToFile(binaryBlob, fileName);
+      const payload = new FormData();
+      payload.append('media', binaryBlob, fileName);
+
+      this.apiService.uploadImage(payload)
+      .subscribe(result => {
+        console.log(result);
+        self.list_image_select = [];
+        self.list_image_select.push(result);
+        self.sendAttachmentImage();
+      },
+        error => {
+      });
+    }, (err) => {
+      // Handle error
+      // alert(err);
+    });
+  }
+
+  private convertBase64ToBlob(base64: string) {
+    const info = this.getInfoFromBase64(base64);
+    const sliceSize = 512;
+    const byteCharacters = window.atob(info.rawBase64);
+    const byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      const slice = byteCharacters.slice(offset, offset + sliceSize);
+      const byteNumbers = new Array(slice.length);
+
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      byteArrays.push(new Uint8Array(byteNumbers));
+    }
+
+    return new Blob(byteArrays, { type: info.mime });
+  }
+
+  private getInfoFromBase64(base64: string) {
+    const meta = base64.split(',')[0];
+    const rawBase64 = base64.split(',')[1].replace(/\s/g, '');
+    const mime = /:([^;]+);/.exec(meta)[1];
+    const extension = /\/([^;]+);/.exec(meta)[1];
+
+    return {
+      mime,
+      extension,
+      meta,
+      rawBase64
+    };
   }
 }
